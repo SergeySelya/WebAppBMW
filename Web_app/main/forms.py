0236d2fb8b996fb918fd40181d4fd0855f017a0b
SergeySelya <elhom99@gmail.com>
2021-11-22 19:52:00 +0300

    update form

--- Web_app/main/forms.py
+++ Web_app/main/forms.py
@@ -33,6 +33,11 @@ class ServiceFormForm(ModelForm):
             "time": TimeInput(attrs={'placeholder': 'время', 'type': 'time', 'class': 'field__input'}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super(ServiceFormForm, self).__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance and instance.pk:
+            self.fields['type_work'].disabled = True
 
 class CreateUserForm(UserCreationForm):
     class Meta:
